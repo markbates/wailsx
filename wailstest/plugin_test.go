bb4f6348ab8a580c82b51fbf903f7bc3b28db983
Mark Bates <mark@markbates.com>
2024-03-06 13:29:02 -0500

    moved root level code until i'm ready for it

--- wailstest/plugin_test.go
+++ wailstest/plugin_test.go
@@ -2,15 +2,12 @@ package wailstest_test
 
 import (
 	"testing"
-
-	"github.com/markbates/wailsx"
-	. "github.com/markbates/wailsx/wailstest"
 )
 
 func Test_Plugin_Implementations(t *testing.T) {
 	t.Parallel()
 
-	var _ wailsx.Saver = &SaverPlugin{}
-	var _ wailsx.Shutdowner = &ShutdownerPlugin{}
-	var _ wailsx.Startuper = &StartuperPlugin{}
+	// var _ wailsx.Saver = &SaverPlugin{}
+	// var _ wailsx.Shutdowner = &ShutdownerPlugin{}
+	// var _ wailsx.Startuper = &StartuperPlugin{}
 }
